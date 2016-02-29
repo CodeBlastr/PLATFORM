98357b51547a0df07fd136cdc3cd3c4d786e9546
CodeBlastr <github@razorit.com>
2016-02-29 18:01:30 -0500

    added plugin to loadAll()

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -182,9 +182,9 @@ Request::addDetector('tablet', function ($request) {
 
  // this really sucks, that this has to be here like this
 Plugin::loadAll([
-	'CodeBlastr/Skeleton' => ['routes' => true],
+    'Acl', ['bootstrap' => true],
 	'Privileges' => ['routes' => true],
-	'Acl', ['bootstrap' => true]
+    'Users', ['routes' => true]
 	]); // Loads all plugins at once
 //Plugin::load('Migrations');
 
