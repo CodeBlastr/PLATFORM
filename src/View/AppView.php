ac654f71641b40b2db910b56ce3a6df97fdde6ef
CodeBlastr <github@razorit.com>
2016-03-07 22:36:43 -0500

    installed a bootstrap plugin, and updated appview to use it

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -36,8 +36,36 @@ class AppView extends View
      */
     public function initialize()
     {
-        $this->loadHelper('Form', [
-            'templates' => 'bootstrap-forms'
-        ]);
+//        $this->loadHelper('Form', [
+//            'templates' => 'bootstrap-forms'
+//        ]);
+
+        $this->loadHelper('Less', ['className' => 'Less.Less']);
+        $this->loadHelper('Html', ['className' => 'BootstrapUI.Html']);
+        $this->loadHelper('Form', ['className' => 'BootstrapUI.Form', 'templates' => 'bootstrap-forms']);
+        $this->loadHelper('Flash', ['className' => 'BootstrapUI.Flash']);
+        $this->loadHelper('Paginator', ['className' => 'BootstrapUI.Paginator']);
+
+        $this->theme = 'Bootstrap';
+        $this->layout = 'Bootstrap.default';
+
+        $this->assign('navbar.top', $this->_defaultNavbar());
+    }
+
+    protected function _defaultNavbar() {
+        return
+<<<HTML
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Login</a></li>
+        <li>
+            <form class="navbar-form navbar-right" role="search" type="get" action="/signup">
+                <button type="submit" class="btn btn-default">Sign Up!</button>
+            </form>
+        </li>
+HTML;
+
     }
 }
