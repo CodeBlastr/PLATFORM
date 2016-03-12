785a9d0d8028a372182963e9bd5c470c9362a267
CodeBlastr <github@razorit.com>
2016-03-12 01:30:05 -0500

    starting to add authentication

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -30,7 +30,31 @@ class AppController extends Controller
         parent::initialize();
 
         $this->loadComponent('RequestHandler');
+
         $this->loadComponent('Flash');
+
+// going to have to have some kind of auth going on site wide I would imagine
+
+
+//        $this->loadComponent('Auth', [
+//            'storage' => 'Memory',
+//            'authenticate' => [
+//                'Form' => [
+//                    'scope' => ['Users.active' => 1]
+//                ],
+//                'ADmad/JwtAuth.Jwt' => [
+//                    'parameter' => 'token',
+//                    'userModel' => 'Users',
+//                    'scope' => ['Users.active' => 1],
+//                    'fields' => [
+//                        'username' => 'id'
+//                    ],
+//                    'queryDatasource' => true // your JWT token (and thus the user information) will NOT contain an id field if you choose to disable the queryDataSource option. This might might break code depending on the presence of an id field but is easily solved by manually adding the id field to the JWT token (below exp in the code above).  http://www.bravo-kernel.com/2015/04/how-to-add-jwt-authentication-to-a-cakephp-3-rest-api/
+//                ]
+//            ],
+//            'unauthorizedRedirect' => false,
+//            'checkAuthIn' => 'Controller.initialize'
+//        ]);
     }
 
     /**
