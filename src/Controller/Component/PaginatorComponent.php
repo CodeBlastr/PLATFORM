783b8b0ea2113bbe37d0ad18628e7696e6c2df70
CodeBlastr <github@razorit.com>
2016-04-01 16:30:50 -0400

    Paginator upgrade to include filtering
    So far we have the equals key working for multiple different use cases. See the tests for examples.

---
+++ src/Controller/Component/PaginatorComponent.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         2.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller\Component;
+
+use Cake\Controller\Component\PaginatorComponent;
+use Cake\Utility\Hash;
+//use Cake\Datasource\QueryInterface;
+//use Cake\Datasource\RepositoryInterface;
+//use Cake\Network\Exception\NotFoundException;
+
+
+class PaginatorComponent extends PaginatorComponent
+{
+    public $finder = ['filter', 'range'];
+
+    /**
+     * Overwriting so you can specify finder from the url.
+     *
+     * See tests and comments on protected methods for examples
+     *
+     * @param $object
+     * @param array $settings
+     * @return \Cake\Datasource\ResultSetInterface
+     */
+    public function paginate($object, array $settings = [])
+    {
+        $settings = $this->_filter($object, $settings);
+        //$settings['finder'] = ['all' => ['conditions' => ['role' => 'creators']]];
+//        $settings['conditions'] = ['email' => 'admin@example.com'];
+        return parent::paginate($object, $settings);
+    }
+
+    /**
+     *
+     * Not sure $object will be needed so I set it to null for the time being
+     *
+     * @param null $object
+     * @param array $settings
+     * @return array
+     */
+    protected function _filter($object = null, array $settings = [])
+    {
+        //$request = $this->_registry->getController()->request;
+        if (!empty($this->request->query['filter'])) {
+            if (is_array($this->request->query['filter'])) {
+                $filters = $this->request->query['filter'];
+            } else {
+                $filters[] = $this->request->query['filter'];
+            }
+        }
+        switch (!empty($filters)) {
+            case 'equals':
+                // filter by database field full value
+                $settings = Hash::merge($settings, $this->_equals($object));
+                break;
+            case 'begins':
+                // filter by starting letter of database field
+                $settings = Hash::merge($settings, $this->_begins($object));
+                break;
+            case 'contains':
+                // filter by any match of a string in a particular field
+                $settings = Hash::merge($settings, $this->_contains($object));
+                break;
+            case 'between':
+                // filter by range of a particular field
+                $settings = Hash::merge($settings, $this->_between($object));
+                break;
+            case 'greater':
+                // filter by range of a particular field
+                $settings = Hash::merge($settings, $this->_greater($object));
+                break;
+            case 'less':
+                // filter by range of a particular field
+                $settings = Hash::merge($settings, $this->_less($object));
+                break;
+            default:
+                $settings = $settings;
+        }
+        return $settings;
+    }
+
+    /**
+     * ###Usage Example
+     * http://some-request/...?filter[]=equals&equals[first_name][]=test&equals[first_name][]=Jane&equals[role]=user
+     *
+     * @param $object
+     * @return array
+     */
+    protected function _equals($object)
+    {
+        $settings = [];
+        if (!empty($this->request->query['equals']) && is_array($this->request->query['equals'])) {
+            foreach ($this->request->query['equals'] as $field => $value) {
+                $settings['conditions'][$field . ' IN'] = $value;
+            }
+        }
+        return $settings;
+    }
+
+}
---
+++ tests/TestCase/Controller/Component/PaginatorComponentTest.php
@@ -0,0 +1,101 @@
+<?php
+namespace App\Test\TestCase\Controller\Component;
+
+use App\Controller\Component\PaginatorComponent;
+use Cake\Controller\Controller;
+use Cake\Controller\ComponentRegistry;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\TestSuite\TestCase;
+
+class PagematronComponentTest extends TestCase
+{
+
+    public $component = null;
+    public $controller = null;
+    public $fixtures = ['app.articles'];
+
+    public function setUp()
+    {
+        parent::setUp();
+        // Setup our component and fake test controller
+        $request = new Request();
+        $response = new Response();
+        $this->controller = $this->getMock(
+            'Cake\Controller\Controller',
+            null,
+            [$request, $response]
+        );
+        $registry = new ComponentRegistry($this->controller);
+        $this->component = new PaginatorComponent($registry);
+    }
+
+    /**
+     * set get query params that should cause the paginate method to deliver the same as if you had set conditions manually
+     *
+     * Example
+     * http://some-request/articles/index?filter[]=equals&equals[author_id]=1
+     */
+    public function testPaginateFilterEqualsSingle()
+    {
+        $unfiltered = $this->controller->paginate('Articles');
+        $this->controller->paginate['conditions'] = ['author_id' => 1];
+        $filtered = $this->controller->paginate('Articles');
+        $this->controller->paginate = [];
+
+        $count = count($unfiltered) - count($filtered);
+        $this->assertTrue($count > 0); // this test is worthless if they're the same
+
+        //
+        $this->controller->request->query = ['filter' => 'equals', 'equals' => ['author_id' => 1]];
+
+        $this->assertTrue($count == count($unfiltered) - count($this->controller->paginate('Articles')));
+    }
+
+    /**
+     * set get query params that should cause the paginate method to deliver the same as if you had set conditions manually
+     *
+     * Example
+     * http://some-request/articles/index?filter[]=equals&equals[author_id][]=1&equals[author_id][]=4
+     */
+    public function testPaginateFilterEqualsSameFieldDifferentValues()
+    {
+        $unfiltered = $this->controller->paginate('Articles');
+        $this->controller->paginate['conditions'] = ['author_id IN' => [1, 4]];
+        $filtered = $this->controller->paginate('Articles');
+        $this->controller->paginate = [];
+        $count = count($unfiltered) - count($filtered);
+        $this->assertTrue($count > 0); // this test is worthless if they're the same
+
+        // set query params that should cause url get params to deliver the same as if you had set conditions manually
+        $this->controller->request->query = ['filter' => 'equals', 'equals' => ['author_id' =>[1, 4]]];
+        $this->assertTrue($count == count($unfiltered) - count($this->controller->paginate('Articles')));
+    }
+
+    /**
+     * set get query params that should cause the paginate method to deliver the same as if you had set conditions manually
+     *
+     * Example
+     * http://some-request/articles/index?filter[]=equals&equals[author_id][]=4&equals[published]=N
+     */
+    public function testPaginateFilterEqualsDifferentFields()
+    {
+        $unfiltered = $this->controller->paginate('Articles');
+        $this->controller->paginate['conditions'] = ['author_id IN' => 4, 'published IN' => 'N'];
+        $filtered = $this->controller->paginate('Articles');
+        $this->controller->paginate = [];
+        $count = count($unfiltered) - count($filtered);
+        $this->assertTrue($count > 0); // this test is worthless if they're the same
+
+        // set query params that should cause url get params to deliver the same as if you had set conditions manually
+        $this->controller->request->query = ['filter' => 'equals', 'equals' => ['author_id' => 4, 'published' => 'N']];
+        $this->assertTrue($count == count($unfiltered) - count($this->controller->paginate('Articles')));
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        // Clean up after we're done
+        unset($this->component, $this->controller);
+    }
+}
